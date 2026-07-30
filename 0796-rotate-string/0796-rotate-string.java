50b0e52695c9e32299ddc7a0963b3fd9db6408f5
U V M SHANKAR REDDY <manishankarudumula@gmail.com>
2026-07-30 11:48:30 +0530

    Time: 4 ms (2.88%), Space: 43.8 MB (10.79%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+
+        if (s.length() != goal.length()) {
+            return false;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+
+            s = s.substring(1) + s.charAt(0);
+
+            if (s.equals(goal)) {
+                return true;
+            }
+        }
+
+        return s.equals(goal);
+    }
+}
